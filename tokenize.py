a2d10d9e97f6f5630a53aa49e781f32f94e37d56
Daniel Baird <JamesDanielBaird@gmail.com>
2016-10-17 14:06:24 -0700

    Adding example program

--- tokenize.py
+++ tokenize.py
@@ -169,4 +169,29 @@ def tokenize(text):
 
     return tokens
 
-# print(tokenize(program))
+if __name__ == "__main__":
+
+    program = """
+    VAR x, squ;
+
+    /*
+        This is a comment
+    */
+
+    PROCEDURE square;
+    BEGIN
+       squ:= x * x
+    END;
+
+    BEGIN
+       x := 1;
+       WHILE x <= 10 DO
+       BEGIN
+          CALL square;
+          ! squ;
+          x := x + 1
+       END
+    END.
+    """
+
+    print(tokenize(program))
